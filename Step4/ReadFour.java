aebed9291d0ea14e7d18e8cac68ff0aae4410b63
Michael <michael@Zipcoders-MacBook-Pro-4.local>
2024-03-12 22:27:31 -0400

    Finished

--- Step4/ReadFour.java
+++ Step4/ReadFour.java
@@ -1,8 +1,7 @@
 package Step4;
 
+import java.io.*;
 import java.util.Scanner;
-import java.io.File;
-import java.io.IOException;
 
 public class ReadFour
 {
@@ -10,22 +9,20 @@ public class ReadFour
      * Yes, you can look at the other 
      * examples in this lab to build your solution here.
      */
-        public static void main(String[] args)
+        public static void main(String[] args) throws FileNotFoundException
         {
+            File fileIn = new File("/Users/michael/Desktop/Projects/FileIOJava/Step4/input.txt");
+            BufferedReader br = new BufferedReader(new FileReader(fileIn));
+            String str;
+            int sum = 0;
             try {
-            Scanner fileIn = new Scanner(new File("/Users/michael/Desktop/Projects/FileIOJava/Step4/input.txt"));
-            String line;
-            while (fileIn.hasNext())
+
+            while ((str = br.readLine()) != null)
             {
-                // Reads the entire line as a string
-                String lineIn = fileIn.nextLine();
-                // Split the line into a String array
-                String[] input = lineIn.split(",");
-                int sum = 0;
-                for (int i = 0; i < input.length; i ++) {
-                    sum += Integer.parseInt(input[i]);
-                }
-                System.out.println(sum);
+                
+                sum += Integer.parseInt(str);
+
+                System.out.println("Current sum: " + sum);
             }
         }
         catch (IOException e) {
